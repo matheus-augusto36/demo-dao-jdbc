6f37c42da8856ea01ae74caa901863cfed735a65
Matheus Augusto <silvamatheusaugusto36@gmail.com>
2022-05-31 19:31:31 -0300

    Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,24 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date date = sdf.parse("07/11/2003");
 		
 		Department dep = new Department(5, "livros");
+		Seller seller = new Seller(5, "carlos", "carlos@gmail.com", date, 5000.0, dep);
 		
 		System.out.println(dep);
+		System.out.println(seller);
 
 	}
 
